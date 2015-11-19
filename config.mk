bb688ecc39b6030afae93a91cb7298206d434f82
cv <cv@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2015-11-19 21:11:47 +0000

    added config.mk to the repo to avoid downloading it separately
    git-svn-id: http://web.barrett.com/svn/btclient/branches/proficio_p3@766 3eb8b4e9-0710-d544-b5f2-d3973d62577e

---
+++ config.mk
@@ -0,0 +1,21 @@
+# config.mk -- Configuration details
+
+# Define the target operating system
+# TARGET_OS = rtai
+# TARGET_OS = xenomai
+
+TARGET_OS=xenomai
+
+# Define the CANbus hardware installed
+# CAN_HARDWARE = esd
+# CAN_HARDWARE = peak
+# CAN_HARDWARE = socket
+
+CAN_HARDWARE=socket
+
+# Define the type of CAN interface
+# CAN_TYPE = isa
+# CAN_TYPE = pci
+
+CAN_TYPE=pci
+
